47c03a24ffa2b0a96bc44dbf19d1a7fef62f04c2
Brandon <brandon.slater@codeminers.co.uk>
2020-07-01 12:59:24 +0100

    Updated Tests

--- src/FeatureTogglesCoreTests/IPAddressRangeTests.cs
+++ src/FeatureTogglesCoreTests/IPAddressRangeTests.cs
@@ -3,30 +3,61 @@ using System;
 
 namespace FeatureTogglesCoreTests
 {
+    using System;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Net;
+    using FeatureToggles.Models;
+    using NUnit.Framework;
+
+    [TestFixture]
+    [ExcludeFromCodeCoverage]
     public class IPAddressRangeTests
     {
         [Test]
         public void ParseCidrBlockTest()
         {
-            throw new NotImplementedException();
+            string ip = "127.10.0.0/28";
+            IPAddressRange range = IPAddressRange.FromCidrAddress(ip);
+
+            IPAddress.TryParse("127.10.0.0", out IPAddress lower);
+            IPAddress.TryParse("127.10.0.15", out IPAddress higher);
+
+            Assert.IsFalse(IPAddressRange.IsNullOrEmpty(range));
+            Assert.AreEqual(lower, range.Lower);
+            Assert.AreEqual(higher, range.Upper);
+            Console.Error.WriteLine(range.ToString());
         }
 
         [Test]
         public void Foo()
         {
-            throw new NotImplementedException();
+            string ip = "127.0.0.1/28";
+            IPAddressRange range = IPAddressRange.FromCidrAddress(ip);
+
+            Assert.IsFalse(IPAddressRange.IsNullOrEmpty(range));
+            Console.Error.WriteLine(range.ToString());
         }
 
         [Test]
         public void IPAddressInRangeTest()
         {
-            throw new NotImplementedException();
+            string ip = "127.10.0.0/28";
+            IPAddressRange range = IPAddressRange.FromCidrAddress(ip);
+
+            IPAddress.TryParse("127.10.0.1", out IPAddress candidate);
+
+            Assert.IsTrue(range.IPInRange(candidate));
         }
 
         [Test]
         public void IPAddressNotInRangeTest()
         {
-            throw new NotImplementedException();
+            string ip = "127.10.0.0/28";
+            IPAddressRange range = IPAddressRange.FromCidrAddress(ip);
+
+            IPAddress.TryParse("192.168.0.1", out IPAddress candidate);
+
+            Assert.IsFalse(range.IPInRange(candidate));
         }
     }
 }
